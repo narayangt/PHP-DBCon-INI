4afd5dc1d0468b2e83119f111e3e0ea8ad58617d
Narayan Bhandari <narayan.mail12@gmail.com>
2016-09-12 19:34:13 +1000

    Add files via upload

---
+++ db_config.ini.php
@@ -0,0 +1,17 @@
+;<?php
+;die()
+;/*############# /home/planeton/public_html/SCAPE/registry/db_config.ini.php ###############
+[db]
+username=planeton_narayan
+password=Narayan123
+dbname=planeton_maindb
+[dsn]
+driver=mysql
+host=localhost
+port=3306
+[db_options]
+PDO::MYSQL_ATTR_INIT_COMMAND=set names utf8
+[db_attributes]
+PDO::ATTR_ERRMODE=PDO::ERRMODE_EXCEPTION
+
+; end of config */
